b9fb5137367a5a274222a1aa08c80f305885a555
Bora Guven <guvenbe@gmail.com>
2019-08-22 01:08:21 -0400

    react

---
+++ src/main/resources/static/precache-manifest.0be7e3ded53dd41a3c2322e2e9db38f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a714f04b5d573672ec41f8281c6d04eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d5e1158e97fc970ff15",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "790ab219e58c30720bab",
+    "url": "/static/css/main.f761ed1a.chunk.css"
+  },
+  {
+    "revision": "7d5e1158e97fc970ff15",
+    "url": "/static/js/2.1e73332a.chunk.js"
+  },
+  {
+    "revision": "790ab219e58c30720bab",
+    "url": "/static/js/main.a1b8caf6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
